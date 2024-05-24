6585e3cecce3f43651e18ed4b1a5b2c5454bf6d7
Ganesh <ganeshskamble1997@gmail.com>
2024-05-24 12:11:29 +0530

    new updation integration code comment updation 2

--- Sample_RESTlet.js
+++ Sample_RESTlet.js
@@ -24,6 +24,7 @@ define(['N/http', 'N/https', 'N/record', 'N/search'],
                 message: "GET Success"
             }
             //hhyugffyh
+            //New ChangesS
         }
 
         /**
